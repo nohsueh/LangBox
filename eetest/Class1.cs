d74c1761697d556e186a8dd4b09d40b628540dcf
xuexiang <hsuehhsiang@163.com>
2022-05-31 12:47:00 +0800

    创建class1

---
+++ eetest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBox.eetest
+{
+    internal class Class1
+    {
+        void Test()
+        {
+            Console.WriteLine("aoifh");
+        }
+    }
+}
