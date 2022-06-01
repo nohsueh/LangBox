3fd6c8d6235f9935092b5ec6039cf3f4f87a284f
xuexiang <hsuehhsiang@163.com>
2022-06-01 10:43:55 +0800

    add UpdateCheck module

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace LangBox
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task(() =>
+            {
+                bool hasUpdate = UpdateChecker.HasUpdate();
+                if (hasUpdate)
+                {
+                    MessageBoxResult r = MessageBox.Show("发现更新，这个版本可能已经不可以用了喵\n是否前往更新？", "更新提示", MessageBoxButton.YesNo);
+                    if (r == MessageBoxResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start("https://github.com/SDchao/AutoVsCEnv_WPF/releases/latest");
+                    }
+                }
+            });
+            task.Start();
+            this.Content = new Welcome();
+        }
     }
 }
---
+++ Operaters/UpdateChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LangBox.Operaters
+{
+    internal class UpdateChecker
+    {
+        private const string version = "0.001";
+        private const string checkPage = "https://gitee.com/SDchao/AutoVsCEnv_WPF/raw/master/AutoVsCEnv_WPF/Operators/UpdateChecker.cs";
+
+        public static bool HasUpdate()
+        {
+            string content = ReadHttpSourceCode(checkPage);
+            Regex regex = new Regex("private const string version = \"(.*)\"");
+            Match match = regex.Match(content);
+            if (match.Success)
+            {
+                string nowVersion = match.Groups[1].Value;
+                if (double.Parse(version) < double.Parse(nowVersion))
+                    return true;
+            }
+            return false;
+        }
+
+        private static string ReadHttpSourceCode(string url)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.Method = "GET";
+            request.Timeout = 3000;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.68";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(new BufferedStream(response.GetResponseStream()), Encoding.UTF8);
+            string content = reader.ReadToEnd();
+            response.Close();
+            reader.Close();
+            return content;
+        }
+    }
+}
