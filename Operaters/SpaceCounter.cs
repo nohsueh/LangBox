8f4ee1549aafdcc575627d3ac2a9d113b2d2df39
xuexiang <hsuehhsiang@163.com>
2022-06-23 17:17:33 +0800

    update

---
+++ Operaters/SpaceCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBox.Operaters
+{
+    internal class SpaceCounter
+    {
+        //返回所需的总空间
+        public static int SpaceRequired(Dictionary<string, bool> LangMap)
+        {
+            int TotalSpace = 0;
+            foreach(var kvp in LangMap)
+            {
+                TotalSpace += kvp.Value ? 1 : 0;
+            }
+            return TotalSpace;
+        }
+    }
+}
--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Ookii.Dialogs.Wpf;
+﻿using LangBox.Operaters;
+using Ookii.Dialogs.Wpf;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -13,6 +15,7 @@ namespace LangBox.Pages
     public partial class MainPage : Page
     {
         public static string GitHubPath = "https://github.com/NOhsueh/LangBox";
+        public static Dictionary<string, bool> LangMap = new Dictionary<string, bool>();
 
         public MainPage()
         {
@@ -28,19 +31,48 @@ namespace LangBox.Pages
         private void SelectAll_Checked(object sender, RoutedEventArgs e)
         {
             bool flag = SelectAll.IsChecked == true ? true : false;
-            for (int i = 0; i < LangSelect.Children.Count; i++)
+            foreach (var item in LangSelect.Children)
             {
-                var item = LangSelect.Children[i];
                 if (item is CheckBox)
                 {
                     CheckBox checkBoxItem = (CheckBox)item;
                     checkBoxItem.IsChecked = flag;
                 }
             }
+            LangSelect_Click(sender, e);
         }
 
         //检查勾选的语言
+        private void LangSelect_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in LangSelect.Children)
+            {
+                if (item is CheckBox)
+                {
+                    CheckBox checkBoxItem = (CheckBox)item;
+                    string LMkey = checkBoxItem.Name;
+                    bool LMvalue = (bool)checkBoxItem.IsChecked;
 
+                    if (LangMap.ContainsKey(LMkey))
+                    {
+                        LangMap[LMkey] = LMvalue;
+                    }
+                    else
+                    {
+                        LangMap.Add(LMkey, LMvalue);
+                    }
+                }
+            }
+
+            SpaceRequiredShow();
+        }
+
+        //改变所需总空间显示
+        private void SpaceRequiredShow()
+        {
+            int TotalSpace = SpaceCounter.SpaceRequired(LangMap);
+            SpaceRequired.Text = TotalSpace.ToString() + "MB space required";
+        }
 
         //点击浏览文件
         private void Browse_Click(object sender, RoutedEventArgs e)
