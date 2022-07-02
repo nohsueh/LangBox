5dc95f564d9ad65226849851c15b2647a434902b
xuexiang <hsuehhsiang@163.com>
2022-07-02 21:58:07 +0800

    update

--- Operaters/ExtractHelper.cs
+++ Operaters/ExtractHelper.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System.IO.Compression;
 
 namespace LangBox.Operaters
 {
     internal class ExtractHelper
     {
-        
+        public static void UnZipFile(string targetFile,string savePath)
+        {
+            ZipFile.ExtractToDirectory("D:\\a.zip", "D:\\");
+        }
     }
 }
